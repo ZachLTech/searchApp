6ca68dee779ee50108fed6f20925940af9798a9a
ZachLTech <zlopezzl2021@gmail.com>
2023-06-22 02:08:52 -0400

    modified:   package-lock.json 	modified:   package.json 	modified:   src/routes/searchBar.svelte

--- src/routes/searchBar.svelte
+++ src/routes/searchBar.svelte
@@ -8,6 +8,8 @@ const svelteTest = httpsCallable(functions, 'svelteTest');
 
 let result = "Search";
 async function callCloudFunction() {
+  const functions = getFunctions();
+  const svelteTest = httpsCallable(functions, 'svelteTest');
 
 }
 
@@ -32,6 +34,6 @@ async function searchAPI(){
 </script>
 
 <main class="flex flex-col w-screen h-[calc(100vh-6rem)] justify-center items-center gap-4">
-    <div id="myElement" class=" text-7xl font-monstreaf font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-sky-600"><p>The result is: {svelteTest}</p></div>
+    <div id="myElement" class=" text-7xl font-monstreaf font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-sky-600"><p>The result is: {result}</p></div>
     <div class="h-16 w-3/4 relative flex justify-end items-center"><input class=" absolute h-16 w-full bg-white text-5xl font-monstreaf rounded-lg"><button on:click={() => callCloudFunction()} class=" absolute h-20 w-20" ><img alt="icon" src={barIcon} /></button></div>
 </main>
